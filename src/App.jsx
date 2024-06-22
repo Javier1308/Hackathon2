9bb778de3b389637d93782aa252e765096de85a5
flauts <hector.nieto@utec.edu.pe>
2024-06-22 16:54:08 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import AdminView from './components/AdminView';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Log';
+import RegisterPage from './pages/register';
+// import AdminView from './components/AdminView';
 import ProductList from './components/ProductList';
-import ProductDetails from './components/ProductDetails';
-import Cart from './components/Cart';
-
-import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+// import ProductDetails from './components/ProductDetails';
+// import Cart from './components/Cart';
 import './App.css'
-import Log from './pages/Log'
 function App() {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/admin" component={AdminView} />
-        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/register" component={RegisterPage} />
+        {/* <Route path="/admin" component={AdminView} /> */}
+        {/* <Route path="/products/:id" component={ProductDetails} /> */}
         <Route path="/products" component={ProductList} />
-        <Route path="/cart" component={Cart} />
-        <Redirect from="/" to="/products" />
-      </Switch>
+        {/* <Route path="/cart" component={Cart} /> */}
+        <Route path="*" component={<Navigate to="/" />} />
+      </Routes>
     </Router>
   );
 }
